2e4749cea0ad77af92cde7dc3957b282c80ca6ba
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-09-06 01:25:12 +0600

    Add files via upload

--- Testing/EquivalenceTesting/src/NewPackage/InfoValidate.java
+++ Testing/EquivalenceTesting/src/NewPackage/InfoValidate.java
@@ -13,4 +13,56 @@ public class InfoValidate {
 		else if(age>65 && age<80) return true;
 		else return false;
 	}
+	
+	private boolean isLeap(int year)
+	{
+	// Return true if year
+	// is a multiple of 4 and
+	// not multiple of 100.
+	// OR year is multiple of 400.
+	return (((year % 4 == 0) &&
+	         (year % 100 != 0)) ||
+	         (year % 400 == 0));
+	}
+
+	private boolean generaldateCheck(int d, int m, int y) {
+		   if (y > 9999 || y < 1962)
+			    return false;
+			    if (m < 1 || m > 12)
+			    return false;
+			    if (d < 1 || d > 31)
+			    return false;
+			 
+			    // Handle February month
+			    // with leap year
+			    if (m == 2)
+			    {
+			        if (isLeap(y))
+			        return (d <= 29);
+			        else
+			        return (d <= 28);
+			    }
+			 
+			    // Months of April, June,
+			    // Sept and Nov must have
+			    // number of days less than
+			    // or equal to 30.
+			    if (m == 4 || m == 6 ||
+			        m == 9 || m == 11)
+			        return (d <= 30);
+			 
+			    return true;
+	}
+	
+	public boolean dateCheck(int m, int d, int y) {
+		if(generaldateCheck(d,m,y) == true) {
+			if(y==1962 && m>=5 && d <=20)
+				return true;
+			else 
+				return false;
+		}
+		else 
+			return false;
+	}
+	
 }
